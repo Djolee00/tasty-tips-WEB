3d1f24015c594cefc4f900615e38f9ebfe2e9050
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2022-10-31 22:29:41 +0100

    Finished navbar and footer

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {*} selector - css class
+ * @returns first element which has that css class (specified in selector)
+ */
+const getElement = (selector) => {
+
+  const element = document.querySelector(selector);
+  if(element) return element;
+  throw Error(`Please double check your class names, there is no
+               ${selector} class`);
+
+}
+
+
+
+const links = getElement('.nav-links');
+const navBtnDOM = getElement('.nav-btn');
+
+/**
+ * fucntion to add class on nav-links when menu bar button is clicked
+ * (this is used when site is not in full screen)
+ */
+navBtnDOM.addEventListener('click',() => {
+  links.classList.toggle('show-links');
+})
