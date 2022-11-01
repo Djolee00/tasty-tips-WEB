9fab6a11a477d8e2d16db90aa2d7d8cc26da94db
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2022-11-02 00:15:39 +0100

    Finished modal for Add

--- js/app.js
+++ js/app.js
@@ -32,3 +32,44 @@ navBtnDOM.addEventListener('click',() => {
 const date = getElement('#date');
 const currentYear = new Date().getFullYear();
 date.textContent = currentYear;
+
+
+
+function setOverlay(active) {
+  const overlay = document.querySelector('#background-overlay');
+  if (active) {
+      overlay.classList.add('active');
+  } else {
+      overlay.classList.remove('active')
+  }
+}
+
+function closeModals() {
+  setOverlay(false);
+  setAddNewRecipeModal(false);
+  // setEditUserDataModal(false);
+  // setDeleteAdModal(false);
+  // setDeleteProfileModal(false);
+  // setEditAdModal(false);
+}
+
+/**
+ * showing modal for adding recipe
+ */
+
+const addNewRecipe = () => {
+  console.log("Sdasd");
+  setOverlay(true);
+  setAddNewRecipeModal(true);
+  return false;
+}
+
+function setAddNewRecipeModal(active) {
+  const modal = document.querySelector('#addNewRecipeModal');
+  if (active) {
+      modal.classList.add('active');
+  } else {
+      modal.classList.remove('active')
+  }
+}
+
