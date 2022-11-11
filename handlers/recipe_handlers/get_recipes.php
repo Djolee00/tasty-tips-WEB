99836b0c4c341774e68ffab3f9cb6b3ff27c3a18
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2022-11-11 14:47:02 +0100

    Bug fixed for filters

--- handlers/recipe_handlers/get_recipes.php
+++ handlers/recipe_handlers/get_recipes.php
@@ -7,19 +7,19 @@ require '../../models/Recipe.php';
 if (isset($_GET['getAll'])) {
     $recipes = Recipe::getRecipes($conn);
 
-    $res = json_encode($recipes);
+    $res = json_encode(utf8ize($recipes));
     echo  $res;
 }
 
 if (isset($_GET['filter'])) {
-    echo json_encode(Recipe::filterRecipes(
+    echo json_encode(utf8ize(Recipe::filterRecipes(
         $conn,
         sanitizeUserData($_POST['name']),
         $_POST['prepTimeFrom'],
         $_POST['prepTimeTo'],
         $_POST['cookTimeFrom'],
         $_POST['cookTimeTo']
-    ));
+    )));
 }
 
 function sanitizeUserData($data)
@@ -28,4 +28,19 @@ function sanitizeUserData($data)
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
+}
+
+/* Use it for json_encode some corrupt UTF-8 chars
+ * useful for = malformed utf-8 characters possibly incorrectly encoded by json_encode
+ */
+function utf8ize($mixed)
+{
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } elseif (is_string($mixed)) {
+        return mb_convert_encoding($mixed, "UTF-8", "UTF-8");
+    }
+    return $mixed;
 }
